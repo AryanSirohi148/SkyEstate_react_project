cab55afafd6d49870102eda1a7a0b3985a1f23f7
Aryan Sirohi <aryansirohi@Aryans-MacBook-Air.local>
2025-04-19 10:18:24 +0530

    Initial commit - fresh project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='text-3xl font-bold underline' >
+        hello ji
+      </div>
+    </div>
+
+  )
+}
+
+export default App
